281403bcc2d7cbdb143fbe4f6cf437fd00a8390d
Lucas Eduardo <lucas59356@gmail.com>
2020-12-24 14:56:31 -0300

    fix syntax, squash me

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 {pkgs ? import <nixpkgs> {}}:
-in pkgs.buildGoModule rec {
+pkgs.buildGoModule rec {
   name = "dotenv";
   version = "0.0.1";
   vendorSha256 = "0092fdfs73vxf3f9yllxg037i1mgap0x11s5xbs2b0x8s1dl59qm";
